aca961b21cbdca8d946857ae880d50c2d9f6b2f4
jeffihaza <jeffihaza@gmail.com>
2026-06-15 18:50:21 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import {
   Radio, Play, Pause, Upload, Mic2, Headphones, CalendarDays,
